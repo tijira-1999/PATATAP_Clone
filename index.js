95d729b581d5e705521e394abfdc1aed706fe4e1
arijit sahoo <tijira1999sahoo@gmail.com>
2019-08-07 11:29:43 +0530

    stable first build

---
+++ index.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () =>{
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads > div")
+
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].play();
+        });
+    });
+
+// console.log(sounds);
+
+});
